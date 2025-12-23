38608ce3ab5db31fd321c5249eab18e519bf96e9
kys <kys@daeryunens.co.kr>
2025-12-23 16:55:24 +0900

    [ADD] 근무표 수정 모달의 근로모듈 정돈

--- pybo/templatetags/pybo_filter.py
+++ pybo/templatetags/pybo_filter.py
@@ -1,4 +1,7 @@
+import re
 from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
 from dateutil.relativedelta import relativedelta
 from datetime import datetime
 
@@ -101,3 +104,23 @@ def ss_to_hhmmss(value):
     h, rem = divmod(s, 3600)
     m, sec = divmod(rem, 60)
     return f"{h:02d}:{m:02d}:{sec:02d}"
+
+
+@register.filter
+def pretty_module_name(name: str):
+    if not name:
+        return ""
+
+    s = escape(name)
+
+    # 1) "(...)" 는 무조건 다음 줄로: "대진(전일)" -> "대진<br>(전일)"
+    s = re.sub(r"\s*\(", "<br>(", s)
+
+    # 2) 공백이 있으면 공백 기준으로 줄바꿈: "평일 의사" -> "평일<br>의사"
+    s = s.replace(" ", "<br>")
+
+    # 3) 예외/표준화가 필요한 용어는 매핑으로 강제 줄바꿈
+    s = s.replace("경조휴가", "경조<br>휴가")
+    s = s.replace("무급휴가", "무급<br>휴가")
+
+    return mark_safe(s)
