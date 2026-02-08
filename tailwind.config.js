ed54dda251e8da47d073f4b0e92fd92a50da565c
ggomdong <ggomdong@gmail.com>
2026-02-08 18:23:56 +0900

    [MOD] 화면 크기 재정의, 스크립트 오류 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,20 @@
 module.exports = {
   content: [
     "./templates/**/*.html",
-    "./wtm/**/*.py",
-    "./api/**/*.py",
-    "./common/**/*.py",
-    "./config/**/*.py",
+    "./**/templates/**/*.html",
+    "./**/*.py",
   ],
   theme: {
     extend: {},
+    screens: {
+      // mobile-first
+      sm: "640px",
+      md: "768px",     // 모바일 기준(햄버거)
+      lg: "1024px",    // 중간(상단네비)
+      xl: "1280px",
+      "2xl": "1536px",
+      fhd: "1920px",   // ✅ 너희 기존 기준 (좌측 사이드바)
+    },
   },
   plugins: [],
 };
