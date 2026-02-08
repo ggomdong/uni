814654437b3f1411f4b8ef3b51e75ffd1ed97c25
ggomdong <ggomdong@gmail.com>
2026-02-08 18:12:58 +0900

    [ADD] tailwind, gitignore

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./templates/**/*.html",
+    "./wtm/**/*.py",
+    "./api/**/*.py",
+    "./common/**/*.py",
+    "./config/**/*.py",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
